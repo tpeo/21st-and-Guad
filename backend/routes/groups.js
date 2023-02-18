07ed622254a2dc235c5a85af9dd5f5fff99350a1
Vincent Do <vincentvietdo@gmail.com>
2023-02-17 22:12:05 -0600

    starting groups

--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -6,9 +6,16 @@ const cors = require("cors");
 const groups = express.Router();
 groups.use(cors());
 
+groups.post("/", async (req, res) => {
+    console.log("Inside groups GET route!");
+    res.json("Inside groups GET route!");
+  });
+
 groups.get("/", async (req, res) => {
-  console.log("Inside groups GET route!");
-  res.json("Inside groups GET route!");
+    const groups_db = db.collection("groups");
+    const groups = await groups_db.get();
+    const ret = groups.docs.map((obj) => obj.data());
+    res.json(ret);
 });
 
 module.exports = groups;
