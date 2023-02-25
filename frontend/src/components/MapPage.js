12c49778cca2677b7a0697f28e8d26026277786a
Vincent Do <vincentvietdo@gmail.com>
2023-02-24 19:27:59 -0600

    first iteration of map page

--- frontend/src/components/MapPage.js
+++ frontend/src/components/MapPage.js
@@ -1,33 +1,18 @@
-import React, { useState } from "react";
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import Container from '@mui/material/Container';
-import AdbIcon from "@mui/icons-material/Adb";
-import Box from '@mui/material/Box';
-import Tooltip from '@mui/material/Tooltip';
-import Avatar from '@mui/material/Avatar';
-import MenuIcon from '@mui/icons-material/Menu';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, {  } from "react";
+import { ThemeProvider } from '@mui/material/styles';
 import {appTheme} from "./Theme.js";
-import HiveIcon from '@mui/icons-material/Hive';
-import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 import NavBar from "./NavBar.js";
+import MapComponent from "./maps/MapComponent.js";
 
 // react.school/material-ui
-
 
 function MapPage() {
  
   return (
     <ThemeProvider theme={appTheme}>
         <NavBar></NavBar>  
-        map
-
+        <MapComponent apiKey={process.env.REACT_APP_MAPS_API_KEY}></MapComponent>
     </ThemeProvider>
-  
   );
 }
 
