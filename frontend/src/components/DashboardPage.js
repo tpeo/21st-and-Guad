56f8c8a8e128b08247510a17f2328e29a5f58b7d
Vincent Do <vincentvietdo@gmail.com>
2023-03-05 01:38:09 -0600

    added speedial button + moved dialog to dashboard

--- frontend/src/components/DashboardPage.js
+++ frontend/src/components/DashboardPage.js
@@ -1,43 +1,106 @@
 import React, { useState } from "react";
-import AppBar from "@mui/material/AppBar";
-import Select from "@mui/material/Select";
-import Typography from "@mui/material/Typography";
-import InputLabel from "@mui/material/InputLabel";
-import IconButton from "@mui/material/IconButton";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import TextField from "@mui/material/TextField";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardContent from "@mui/material/CardContent";
-import InputAdornment from "@mui/material/InputAdornment";
-import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Select,
+  Typography,
+  InputLabel,
+  IconButton,
+  MenuItem,
+  FormControl,
+  TextField,
+  Avatar,
+  Button,
+  Box,
+  Grid,
+  Card,
+  CardHeader,
+  CardContent,
+  InputAdornment,
+  SpeedDial,
+  SpeedDialAction,
+  SpeedDialIcon,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Zoom,
+  Slide,
+} from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { appTheme } from "./Theme.js";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import {
+  Add as AddIcon,
+  Save,
+  Hive,
+  GroupAdd,
+  MenuIcon,
+  ExpandMore as ExpandMoreIcon,
+  AccountCircleRounded as AccountCircleRoundedIcon,
+} from "@mui/icons-material";
 import NavBar from "./NavBar.js";
+import HousingDialogContent from "./cards/HousingDialogContent.js";
 
 // react.school/material-ui
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  //return <Slide direction="up" ref={ref} {...props} />;
+  return <Zoom in ref={ref} {...props} />;
+});
+
 function DashboardPage() {
-  const [val, setVal] = useState(1);
+  const [val, setVal] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleDialogClose = () => {
+    setOpenDialog(false);
+  };
+
+  const handleCreateHousingCard = (housingCardData) => {
+    console.log(housingCardData); // handle the submitted housing card data here
+    handleClose();
+  };
 
   const handleChange = (event) => {
     setVal(event.target.value);
   };
 
-  const iconComponent = (props) => {
-    return;
-  };
-
   const data = [
     { name: "Lark", address: "2100 Nueces Street, Austin, Texas" },
     { name: "Ion", address: "2100 San Antonio St, Austin, Texas" },
+  ];
+
+
+  const actions = [
+    {
+      icon: <AddIcon />,
+      name: "Add Apartment Card",
+      tooltipTitle: "Add Apartment",
+      onClick: () => {
+        console.log("add apartment");
+        setOpenDialog(true);
+      },
+    },
+    {
+      icon: <GroupAdd />,
+      name: "Add Group Member",
+      tooltipTitle: "Add Group Member",
+      onClick: () => {
+        console.log("add group member");
+      },
+    },
+    {
+      icon: <Save />,
+      name: "Save",
+      tooltipTitle: "Save",
+      onClick: () => {
+        console.log("save");
+      },
+    },
   ];
 
   return (
@@ -73,14 +136,13 @@ function DashboardPage() {
           <Box component="form" noValidate sx={{ width: 350, my: 2 }}>
             <FormControl>
               <Select
-                sx={{ml: 14}}
-                disableUnderline
+                sx={{ ml: 14 }}
+                //disableunderline
                 IconComponent={ExpandMoreIcon}
                 value={val}
                 onChange={handleChange}
               >
                 <MenuItem value={0}>Roommate 1</MenuItem>
-               
               </Select>
             </FormControl>
           </Box>
@@ -111,10 +173,43 @@ function DashboardPage() {
             </Grid>
           ))}
         </Grid>
-            
-        {/* floating action button w/ multiple options */}
-        
+
+        <SpeedDial
+          ariaLabel="SpeedDial example"
+          sx={{ position: "fixed", bottom: 16, right: 16 }}
+          icon={<SpeedDialIcon openIcon={<Hive />}/>}
+          onClose={() => setOpen(false)}
+          onOpen={() => setOpen(true)}
+          direction="left"
+          open={open}
+        >
+          {actions.map((action) => (
+            <SpeedDialAction
+              key={action.name}
+              icon={action.icon}
+              tooltipTitle={action.tooltipTitle}
+              onClick={action.onClick}
+            />
+          ))}
+        </SpeedDial>
       </Grid>
+      {/* dialog that can go anywhere on the page bc it pops up from middle */}
+      <Dialog
+          open={openDialog}
+          onClose={handleDialogClose}
+          TransitionComponent={Transition}
+          keepMounted
+          maxWidth="lg"
+          scroll="paper"
+        >
+          <DialogContent>
+            <HousingDialogContent
+              onSubmit={handleCreateHousingCard}
+              onClose={handleDialogClose}
+            />
+          </DialogContent>
+          <DialogActions></DialogActions>
+        </Dialog>
     </ThemeProvider>
   );
 }
--- frontend/src/components/MeetingPage.js
+++ frontend/src/components/MeetingPage.js
@@ -2,14 +2,13 @@ import React, { useState } from "react";
 import { ThemeProvider } from "@mui/material";
 import { appTheme } from "./Theme";
 import NavBar from "./NavBar";
-import HousingDialogHandler from "./cards/HousingDialogHandler";
 
 function MeetingPage() {
   return (
     <ThemeProvider theme={appTheme}>
       <NavBar></NavBar>
       {/* meeting */}
-      <HousingDialogHandler></HousingDialogHandler>
+      Meeting
     </ThemeProvider>
   );
 }
