7b8650851cf03d06473d4afd6311db74b2f74e14
Vincent Do <vincentvietdo@gmail.com>
2023-03-05 03:27:04 -0600

    prelim logo changes on login/register

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -2,177 +2,199 @@ import React, { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
 import "./ApartmentForm.css";
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
 import AuthContext from "../contexts/AuthContext";
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {appTheme} from "./Theme.js";
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { appTheme } from "./Theme.js";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 
 function LoginPage() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [formData, setFormData] = useState({
-        email: "",
-        password: "",
-    });
+  // Use state to manage error messages
+  const [errorMessage, setErrorMessage] = useState("");
 
-    // Use state to manage error messages
-    const [errorMessage, setErrorMessage] = useState("");
+  // Use the useNavigate hook from react-router to navigate between pages
+  const navigate = useNavigate();
 
-    // Use the useNavigate hook from react-router to navigate between pages
-    const navigate = useNavigate();
+  // Use the AuthContext to access the authentication functions (loginUser and createUser)
+  const auth = useContext(AuthContext);
 
-    // Use the AuthContext to access the authentication functions (loginUser and createUser)
-    const auth = useContext(AuthContext);
+  // useEffect will run on first render and any time auth.loggedIn changes
+  useEffect(() => {
+    // Check if the user is logged in
+    if (auth.loggedIn) {
+      // If the user is logged in, redirect to the apartment page
+      navigate("/dashboard");
+    }
+  }, []);
 
-    // useEffect will run on first render and any time auth.loggedIn changes
-    useEffect(() => {
-        // Check if the user is logged in
-        if (auth.loggedIn) {
-        // If the user is logged in, redirect to the apartment page
+  // Event handler for the login button, it calls the loginUser
+  // function from the AuthContext and navigates to the main page
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log(formData);
+    if (!formData.email || !formData.password) {
+      setErrorMessage("All fields are required.");
+    } else {
+      try {
+        await auth.loginUser(formData.email, formData.password);
         navigate("/dashboard");
-        }
-    }, []);
+        setErrorMessage("");
+      } catch (error) {
+        setErrorMessage(error);
+      }
+    }
+  };
 
-    // Event handler for the login button, it calls the loginUser 
-    // function from the AuthContext and navigates to the main page
-    const handleLogin = async (event) => {
-        event.preventDefault();
-        console.log(formData);
-        if (!formData.email || !formData.password) {
-        setErrorMessage("All fields are required.");
-        } else {
-        try {
-            await auth.loginUser(formData.email, formData.password);
-            navigate("/dashboard");
-            setErrorMessage("");
-        } catch (error) {
-            setErrorMessage(error);
-        }
-        }
-    };
+  // Event handler to update the formData state when the input fields change
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    // Event handler to update the formData state when the input fields change
-    const handleChange = (event) => {
-        setFormData({
-        ...formData,
-        [event.target.name]: event.target.value,
-        });
-    };
+  return (
+    <ThemeProvider theme={appTheme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
 
-    return (
-        <ThemeProvider theme={appTheme}>
-        <Grid container component="main" sx={{ height: '100vh'}}>
-          <CssBaseline />
+        {/* left side */}
+        <img
+          src={process.env.PUBLIC_URL + "/images/logoSplash.png"}
+          alt="Logo"
+          style={{
+            top: 0,
+            left: 0,
+            height: "100vh",
+            width: "35vw",
+          }}
+        ></img>
 
-            {/* left side */}
-            <Grid item xs={12} sm={6} md={6} sx = {{backgroundColor: appTheme.palette.secondary.main, borderRadius: 0}} component={Paper}>
-                <Box item xs
-                 sx={{
-                    height: 200,
-                    my: 12,
-                    mx: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    backgroundColor: appTheme.palette.primary.main,
-                  }}
-                  > 
-                </Box>
-            </Grid>
-
-            {/* right side */}
-            <Grid item xs={12} sm={6} md={6} component={Paper} sx={{borderRadius: 0}}>
-
-            <Box
+        {/* right side */}
+        <Grid
+          item
+          // xs={12}
+          // sm={6}
+          // md={6}
+          component={Paper}
+          sx={{ borderRadius: 0,
+            flexGrow: 1,
+            display: "flex",
+            width: "50vw",
+            alignItems: "center",
+            justifyContent: "center", }}
+        >
+          <Box
             sx={{
-                my: 15,
-                mx: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
+              my: 12,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
+          >
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleLogin}
+              sx={{ width: 350, my: 2 }}
             >
-
-            <Box component="form" noValidate onSubmit={handleLogin} sx={{width: 350, my: 2 }}>
-
-              <Typography component="h1" 
-                sx={{fontWeight: appTheme.typography.fontWeightBold, display: 'flex',
-                flexDirection: 'column', alignItems: 'center', fontSize: appTheme.typography.h1.fontSize}}>
-                  Login
+              <Typography
+                component="h1"
+                sx={{
+                  fontWeight: appTheme.typography.fontWeightBold,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  fontSize: appTheme.typography.h1.fontSize,
+                }}
+              >
+                Login
               </Typography>
 
-                {/* implement this later https://developers.google.com/identity/gsi/web/reference/js-reference */}
-                <Button type="submit" fullWidth variant="contained" disableElevation
-                    sx={{ mt: 3, mb: 2, backgroundColor: appTheme.palette.primary.grey2, color: '#000000'}}
-                >
-                  Sign in with Google
-                </Button>
+              {/* implement this later https://developers.google.com/identity/gsi/web/reference/js-reference */}
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                disableElevation
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  backgroundColor: appTheme.palette.primary.grey2,
+                  color: "#000000",
+                }}
+              >
+                Sign in with Google
+              </Button>
 
-                <Typography component="h5" sx={{fontWeight: 600, mb: -1.5}}>
-                  Email
-                </Typography>
+              <Typography component="h5" sx={{ fontWeight: 600, mb: -1.5 }}>
+                Email
+              </Typography>
 
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    name="email"
-                    autoFocus
-                    sx={{mb: 2.5}}
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                name="email"
+                autoFocus
+                sx={{ mb: 2.5 }}
+                value={formData.email}
+                onChange={handleChange}
+              />
 
-                    value={formData.email}
-                    onChange={handleChange}
-                />
+              <Typography component="h5" sx={{ fontWeight: 600, mb: -1.5 }}>
+                Password
+              </Typography>
 
-                <Typography component="h5" sx={{fontWeight: 600,  mb: -1.5}}>
-                  Password
-                </Typography>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                type="password"
+                id="password"
+                sx={{ mb: 2.5 }}
+                inputProps={{ minLength: 6 }}
+                value={formData.password}
+                onChange={handleChange}
+              />
 
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  type="password"
-                  id="password"
-                  sx={{mb: 2.5}}
-                  inputProps={{ minLength: 6 }}
-
-                  value={formData.password}
-                  onChange={handleChange}
-                />
-
-                <Button
-                    onClick={handleLogin}
-                    fullWidth
-                    variant="contained"
-                    disableElevation
-                    sx={{ mt: 3, mb: 2 }}
-                >
-                    Login
-                </Button>
-
+              <Button
+                onClick={handleLogin}
+                fullWidth
+                variant="contained"
+                disableElevation
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Login
+              </Button>
             </Box>
-            
+
             <Link href="/register" variant="body2" underline="none">
-                {"Don't have an account?"} 
-                <Box fontWeight='700' display='inline' color='#000000'> Sign up</Box>
+              {"Don't have an account?"}
+              <Box fontWeight="700" display="inline" color="#000000">
+                {" "}
+                Sign up
+              </Box>
             </Link>
-            
-        </Box>
+          </Box>
         </Grid>
-
-     </Grid>
-     </ThemeProvider>    
-    );
-
+      </Grid>
+    </ThemeProvider>
+  );
 }
 
 export default LoginPage;
--- frontend/src/components/RegisterPage.js
+++ frontend/src/components/RegisterPage.js
@@ -5,51 +5,51 @@ import "./ApartmentForm.css";
 import BasicInfo from "./BasicInfo";
 import AboutInfo from "./AboutInfo";
 import PreferencesInfo from "./PreferencesInfo";
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
 import AuthContext from "../contexts/AuthContext";
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {appTheme} from "./Theme.js";
-
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { appTheme } from "./Theme.js";
+import { width } from "@mui/system";
 
 function RegisterPage() {
+  const [page, setPage] = useState(0);
 
-   const [page, setPage] = useState(0);
+  // Use state to manage error messages
+  const [errorMessage, setErrorMessage] = useState("");
 
-   // Use state to manage error messages
-   const [errorMessage, setErrorMessage] = useState("");
+  // Use the useNavigate hook from react-router to navigate between pages
+  const navigate = useNavigate();
 
-   // Use the useNavigate hook from react-router to navigate between pages
-   const navigate = useNavigate();
+  // Use the AuthContext to access the authentication functions (loginUser and createUser)
+  const auth = useContext(AuthContext);
 
-   // Use the AuthContext to access the authentication functions (loginUser and createUser)
-   const auth = useContext(AuthContext);
-
-   const conditionalComponent = () => {
+  const conditionalComponent = () => {
     switch (page) {
       case 0:
         return <BasicInfo formData={formData} setFormData={setFormData} />;
       case 1:
         return <AboutInfo formData={formData} setFormData={setFormData} />;
-       case 2:
-         return <PreferencesInfo formData={formData} setFormData={setFormData}/>;
-       default:
-         return <BasicInfo formData={formData} setFormData={setFormData}/>;
+      case 2:
+        return (
+          <PreferencesInfo formData={formData} setFormData={setFormData} />
+        );
+      default:
+        return <BasicInfo formData={formData} setFormData={setFormData} />;
     }
-  }; 
-
-
-  //move on to next page
-  function handleNext () {
-    setPage(page + 1);
-    console.log(formData);
   };
 
-  function handleSubmit (event) {
+  //move on to next page
+  function handleNext() {
+    setPage(page + 1);
+    console.log(formData);
+  }
+
+  function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
     if (!formData.email || !formData.password) {
@@ -64,16 +64,16 @@ function RegisterPage() {
         setErrorMessage(error);
       }
     }
-  };
+  }
 
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-    name: '',
-    gender: '',
-    first_preference: '',
-    second_preference: '',
-    third_preference: '',
+    email: "",
+    password: "",
+    name: "",
+    gender: "",
+    first_preference: "",
+    second_preference: "",
+    third_preference: "",
     price: 0,
     distance: 0,
   });
@@ -91,7 +91,7 @@ function RegisterPage() {
       },
       redirect: "follow", // manual, *follow, error
       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify({  
+      body: JSON.stringify({
         email: formData.email,
         name: formData.name,
         gender: formData.gender,
@@ -100,89 +100,106 @@ function RegisterPage() {
         third_preference: formData.third_preference,
         price: formData.price,
         distance: formData.distance,
-      }) 
+      }),
     })
-    .then(resp => resp.json())
-    .then(json => console.log(json))
-    .catch((err) => {
+      .then((resp) => resp.json())
+      .then((json) => console.log(json))
+      .catch((err) => {
         console.log(err);
       });
   }
 
   return (
     <ThemeProvider theme={appTheme}>
-        <Grid container component="main" sx={{ height: '100vh'}}>
-          <CssBaseline />
+      <Grid container component="main" sx={{ height: "100vh"}}>
+        <CssBaseline />
 
-            {/* left side */}
-            <Grid item xs={12} sm={6} md={6} sx = {{backgroundColor: appTheme.palette.secondary.main, borderRadius: 0}} component={Paper}>
-                <Box item xs
-                 sx={{
-                    height: 200,
-                    my: 12,
-                    mx: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    backgroundColor: appTheme.palette.primary.main,
-                  }}
-                  > 
-                </Box>
-            </Grid>
+        {/* left side */}
+        <img
+          src={process.env.PUBLIC_URL + "/images/logoSplash.png"}
+          alt="Logo"
+          style={{
+            top: 0,
+            left: 0,
+            height: "100vh",
+            width: "35vw",
+          }}
+        ></img>
 
-            {/* right side */}
-            <Grid item xs={12} sm={6} md={6} component={Paper} sx={{borderRadius: 0}}>
-
-            <Box
+        {/* right side */}
+        <Grid
+          item
+          // xs="auto"
+          // sm="auto"
+          // md={6}
+          component={Paper}
+          sx={{
+            borderRadius: 0,
+            flexGrow: 1,
+            display: "flex",
+            width: "50vw",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box
             sx={{
-                my: 15,
-                mx: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
+          >
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ width: 350, my: 2 }}
             >
-
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{width: 350, my: 2 }}>
-                {conditionalComponent()}
-                { page === 2 && 
+              {conditionalComponent()}
+              {page === 2 && (
                 <Button
-                    onClick={handleSubmit}
-                    fullWidth
-                    variant="contained"
-                    disableElevation
-                    sx={{ mt: 3, mb: 2 }}
-                    >
-                    Submit
-                </Button>}
-
-                { page < 2 && 
-                <Button
-                    onClick={handleNext}
-                    fullWidth
-                    variant="contained"
-                    disableElevation
-                    sx={{ mt: 3, mb: 2 }}
-                    >
-                    Next
+                  onClick={handleSubmit}
+                  fullWidth
+                  variant="contained"
+                  disableElevation
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Submit
                 </Button>
-                }
+              )}
 
-                {page > 0 && <Button onClick={() => setPage(page - 1)}>Back</Button>}
+              {page < 2 && (
+                <Button
+                  onClick={handleNext}
+                  fullWidth
+                  variant="contained"
+                  disableElevation
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Next
+                </Button>
+              )}
+
+              {page > 0 && (
+                <Button onClick={() => setPage(page - 1)}>Back</Button>
+              )}
             </Box>
 
-            { page === 0 && 
-                <Link href="/login" variant="body2" underline="none">
-                    {"Already have an account?"} 
-                    <Box fontWeight='700' display='inline' color='#000000'> Sign in</Box>
-                </Link>
-            }
-        </Box>
+            {page === 0 && (
+              <Link href="/login" variant="body2" underline="none">
+                {"Already have an account?"}
+                <Box fontWeight="700" display="inline" color="#000000">
+                  {" "}
+                  Sign in
+                </Box>
+              </Link>
+            )}
+          </Box>
         </Grid>
-
-     </Grid>
-     </ThemeProvider>
-
+      </Grid>
+    </ThemeProvider>
   );
 }
 
