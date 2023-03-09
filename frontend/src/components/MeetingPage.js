954313027fdbdbc2b4a88b358772fd5a8a2b6110
Vincent Do <vincentvietdo@gmail.com>
2023-03-08 22:16:01 -0600

    custom amentities icons!

--- frontend/src/components/MeetingPage.js
+++ frontend/src/components/MeetingPage.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import { ThemeProvider } from "@mui/material";
-import { appTheme } from "./Theme";
+import { appTheme, AmenitiesIcon } from "./Theme";
 import NavBar from "./NavBar";
 
 function MeetingPage() {
   return (
     <ThemeProvider theme={appTheme}>
       <NavBar></NavBar>
-      {/* meeting */}
+      {/* meeting */} 
       Meeting
+      <AmenitiesIcon iconName="Wifi" onClick={() => {console.log("button was clicked")}}></AmenitiesIcon>
+      <AmenitiesIcon iconName="Dumbbell" size="small"></AmenitiesIcon>
+      <AmenitiesIcon iconName="Trash" active="true" size="small"></AmenitiesIcon>
+      <AmenitiesIcon iconName="Calendar" active="true"></AmenitiesIcon>
     </ThemeProvider>
   );
 }
--- frontend/src/components/Theme.js
+++ frontend/src/components/Theme.js
@@ -1,48 +1,104 @@
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ButtonBase } from "@mui/material";
+import { useState } from "react";
 
+export const AmenitiesIcon = (props) => {
+  // Set up a state variable for hover
+  const [hover, setHover] = useState(false);
+  const [active, setActive] = useState(props.active);
+
+  return (
+    <ButtonBase
+      style={{
+        // Set the width and height of the button based on the 'size' prop
+        width: props.size === "small" ? "50px" : "75px",
+        height: props.size === "small" ? "50px" : "75px",
+        borderRadius: "50%",
+        alignItems: "center",
+        justifyContent: "center",
+        // Set the background color of the button based on the 'active' prop and the hover state
+        backgroundColor: active
+          ? //active is true
+            hover
+            ? appTheme.palette.primary.light
+            : appTheme.palette.primary.main
+          : //active is false
+            // hover
+            // ? appTheme.palette.primary.darkgrey :
+           appTheme.palette.primary.lightgrey
+      }}
+      // Add event handlers to change the hover state
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      // Add an onClick handler to the button
+      onClick={() => {
+        // Toggle the 'active' state
+        setActive(!active);
+        // Call the 'onClick' prop if it exists (for custom functions)
+        if (props.onClick) {
+          props.onClick();
+        }
+      }}
+    >
+      <img
+        // Set the image source based on the 'iconName' prop
+        src={
+          process.env.PUBLIC_URL +
+          `/images/amenitiesIcons/${props.iconName}.png`
+        }
+        style={{
+          // Set the maximum width and height of the image based on the 'size' prop
+          maxWidth: props.size === "small" ? "30px" : "50px",
+          maxHeight: props.size === "small" ? "30px" : "50px",
+        }}
+        alt={props.iconName}
+      />
+    </ButtonBase>
+  );
+};
 
 export const appTheme = createTheme({
-    palette: {
-        primary: {
-          main: '#EACA78',
-          grey2: '#F5F5F5',
-          white: '#FFFFFF',
-        },
-        secondary: {
-          main: '#D9D9D9',
-        },
-      },
-    root: {
-      
+  palette: {
+    primary: {
+      main: "#EACA78",
+      lightgrey: "#D9D9D9",
+      darkgrey: "#838181",
+      grey2: "#F5F5F5",
+      white: "#FFFFFF",
     },
-    typography: {
-        fontFamily: 'Inter',
-        fontWeightMedium: 500,
-        fontWeightBold: 700,
-        subtitle1: {
-          fontSize: 12,
-        },
-        h1: {
-          fontSize: 23,
-        },
-        button: {
-          textTransform: 'none',
-          fontFamily: 'Inter',
-          fontWeight: 600,
-          height: 45
-        },
-      },
-    shape: {
-      borderRadius: 9,    // defaults to 4
+    secondary: {
+      main: "#D9D9D9",
     },
-    components: {
-      MuiCssBaseline: {
-        styleOverrides: `
+  },
+  root: {},
+  typography: {
+    fontFamily: "Inter",
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+    subtitle1: {
+      fontSize: 12,
+    },
+    h1: {
+      fontSize: 23,
+    },
+    button: {
+      textTransform: "none",
+      fontFamily: "Inter",
+      fontWeight: 600,
+      height: 45,
+    },
+  },
+  shape: {
+    borderRadius: 9, // defaults to 4
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
           @font-face {
             font-family: 'Inter';
             font-style: normal;
           }
         `,
-      }
-    }
-  });
+    },
+  },
+});
